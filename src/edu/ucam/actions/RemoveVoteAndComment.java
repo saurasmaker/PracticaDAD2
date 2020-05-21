b6957060c6d901231befcdea2d1f9391c05612bd
SauraSMakerª <saurasmaker@gmail.com>
2020-05-21 10:25:16 +0200

    .

--- src/edu/ucam/actions/RemoveVoteAndComment.java
+++ src/edu/ucam/actions/RemoveVoteAndComment.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,36 +12,7 @@ import edu.ucam.classes.Vote;
 import edu.ucam.database.LoadData;
 import edu.ucam.database.RemoveElementByReference;
 
-/**
- * Servlet implementation class RemoveVoteAndComment
- */
-@WebServlet("/RemoveVoteAndComment")
 public class RemoveVoteAndComment extends Action {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RemoveVoteAndComment() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
